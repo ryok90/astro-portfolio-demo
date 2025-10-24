c01875277d6cf1ef5009c83c0ce9bbffde49e889
Rodrigo Yokota <ryok90@gmail.com>
2025-10-24 09:22:39 -0300

    fix: add zephyr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import { withZephyr } from "zephyr-astro-integration";
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [withZephyr()],
   adapter: vercel()
 });
